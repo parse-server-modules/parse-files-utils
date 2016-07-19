1a259cdf59ac11d0d93d51200c712ae68bdcde19
Jeremy Pease <jeremy@jeremypease.com>
2016-07-19 08:59:56 -0400

    Resolve configuration file to absolute path before loading

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+var path = require('path');
 var configFilePath = process.argv[2];
 var config = {};
 
 if (configFilePath) {
+  configFilePath = path.resolve(configFilePath);
+
   try {
     config = require(configFilePath);
   } catch(e) {
